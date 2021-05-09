209888a86fdc0bcb2e5900bd7c221c04496898b9
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-05-09 21:23:23 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,28 @@
+//Array of objects
+const quiz = [
+    {
+        q:"I ____ with friends when someone stole their car.",
+        options:["was staying", "stayed", "had stayed"],
+        answer: 0
+    },
+    {
+     q:"The police asked me where I ___ the night before.",
+        options:["was sleeping", "had slept", "slept"],
+        answer: 1   
+    },
+    {
+        q:"When her brother was in prison, she ___ him every month.",
+        options:["had visited", "was visiting", "visited"],
+        answer: 2
+    },
+    {
+        q:"I ___ her after she had spent six months in prison.",
+        options:["didn't recognise", "wasn't recognising", "hadn't recognised"],
+        answer: 0
+    },
+    {
+        q:"___ you ___ the suspect before you saw her in court ?",
+        options:["Did / meet", "Had / met", "Were / meeting"],
+        answer: 1
+    }
+]
